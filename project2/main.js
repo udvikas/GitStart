28c7bf67f69686bcf2ee9391b1f00b4a0d9f4574
Vikash Sharma <kvikashsharma09@gmail.com>
2022-12-05 22:38:29 +0530

    task - 9 is done(filter)

--- project2/main.js
+++ project2/main.js
@@ -15,7 +15,7 @@ function addItem(e) {
     
     // get input value
     const newItem = document.getElementById('item').value;
-
+    const newItem1 = document.getElementById('item1').value;
     // create new li Element
     const ListItems = document.createElement('li');
     
@@ -23,10 +23,13 @@ function addItem(e) {
 
     //add textNode with input value
     let textNode = document.createTextNode(newItem);
-    
+    let textNode1 = document.createTextNode(newItem1);
     // console.log(textNode);
-
-    ListItems.appendChild(textNode);
+    
+        // ListItems.appendChild(textNode);
+        // ListItems.appendChild(textNode1);
+     
+    
    
     // create button element
     const deleteBtn = document.createElement('button');
@@ -35,20 +38,25 @@ function addItem(e) {
     
     const textIndeleteBtn = document.createTextNode('X');
     deleteBtn.appendChild(textIndeleteBtn);
-    ListItems.appendChild(deleteBtn);
-
-    itemList.appendChild(ListItems);
+    
+    // ListItems.appendChild(deleteBtn);
+  
+    // itemList.appendChild(ListItems);
     // console.log(itemList); 
 
     const editBtn = document.createElement('button');
     editBtn.className = 'btn btn-info btn-sm float-right mx-2';
     const editText = document.createTextNode('Edit');
     editBtn.appendChild(editText);
-    ListItems.appendChild(editBtn);
-    for (let i = 0; i < ListItems.length; i++) {
-        itemList.appendChild(ListItems[i]);
+    if (newItem && newItem1) {
+        ListItems.appendChild(textNode);
+        ListItems.appendChild(textNode1);
+        ListItems.appendChild(deleteBtn);
+        ListItems.appendChild(editBtn);
+        itemList.appendChild(ListItems);
+    } else {
+        console.log('nothing');
     }
-    
     console.log(ListItems);
 
 }
@@ -72,12 +80,14 @@ function filterItem(e) {
     
     Array.from(items).forEach(function(item) {
         const itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1) {
+        const itemName1 = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 || itemName1.toLowerCase().indexOf(text) != -1) {
             item.style.display = 'block';
         }else {
             item.style.display = 'none';
         }
         console.log(itemName);
+        console.log(itemName1);
     })
     
 }
